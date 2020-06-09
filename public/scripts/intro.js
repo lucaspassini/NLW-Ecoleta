ad24115c003fc6cf4d837602e42b347d2b5aa577
Lucas Cristiano Passini <passini@protonmail.com>
2020-06-09 04:10:27 -0300

    first commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,75 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar = "Hi"
+// const myconst = "Hi"
+
+// document.write(myconst + myvar)
+
+// string
+// "Olá mundo!"
+
+// number
+// const n1 = "1"
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true or false
+
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objetos possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("Executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Functions - Funcionalidades - Atalhos - Reuso de código
+
+// function sayMyName() {
+// document.write("Lucas")
+// }
+
+// Executar a Function
+// sayMyName()
+
+// OR/AND
+
+//  function sayMyName(name) {
+//    document.write(name)
+//   }
+
+// Executar a Function
+// sayMyName("Lucas")
+// sayMyName("Cristiano")
+// sayMyName("Passini")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5) {
+//    document.write("Reprovado")
+//   } else {
+//     document.write("Aprovado")
+//   }
+
+// Loop repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write('<p>${i}<p>')
+// }
